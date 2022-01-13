4b4a348acf5fa4e4e59a15d439a73769621cfbe8
Zofia Dwojak <z.dwojak.385@studms.ug.edu.pl>
2022-01-13 22:13:42 +0100

    zad1

--- zad1/features/steps/fizzbuzz.py
+++ zad1/features/steps/fizzbuzz.py
@@ -1,5 +1,5 @@
 from behave import *
-from zad1a.FizzBuzz import FizzBuzz
+from zad1.FizzBuzz import FizzBuzz
 
 
 @given("FizzBuzz game")
@@ -9,47 +9,47 @@ def step_impl(context):
 
 @when("3")
 def step_impl(context):
-    context.res = 3
+    context.num = 3
 
 
 @when("5")
 def step_impl(context):
-    context.res = 5
+    context.num = 5
 
 
 @when("15")
 def step_impl(context):
-    context.res = 15
+    context.num = 15
 
 
 @when("7")
 def step_impl(context):
-    context.res = 7
+    context.num = 7
 
 
 @when("20")
 def step_impl(context):
-    context.res = 20
+    context.num = 20
 
 
 @then("Fizz")
 def step_impl(context):
-    assert context.fizzbuzz.game(context.res) == "Fizz"
+    assert context.fizzbuzz.game(context.num) == "Fizz"
 
 
 @then("Buzz")
 def step_impl(context):
-    assert context.fizzbuzz.game(context.res) == "Buzz"
+    assert context.fizzbuzz.game(context.num) == "Buzz"
 
 
 @then("FizzBuzz")
 def step_impl(context):
-    assert context.fizzbuzz.game(context.res) == "FizzBuzz"
+    assert context.fizzbuzz.game(context.num) == "FizzBuzz"
 
 
 @then("7")
 def step_impl(context):
-    assert context.fizzbuzz.game(context.res) == 7
+    assert context.fizzbuzz.game(context.num) == 7
 
 
 
